53475c810211c8cdb9a39bc15f2b63035e53c18d
Marco Sousa <marcosousacontato@gmail.com>
2023-06-14 14:23:00 -0300

    Ajeitando código php

--- Cantina Tia Eliane/frontend/inserir.php
+++ Cantina Tia Eliane/frontend/inserir.php
@@ -1,25 +1,21 @@
-<?php 
-include("/conexao.php");
+<?php
+include("conexao.php");
 
-$nome = $_POST['nome']; 
-
-$email = $_POST['email']; 
-
+$nome = $_POST['nome'];
+$email = $_POST['email'];
 $senha = $_POST['senha'];
 
-$sql = "INSERT INTO usuarios (nome, email, senha)
-        VALUES ('$nome', '$email', '$senha')";
+$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
-$result = $conn->exec($sql); 
+if ($conn->exec($sql)) {
+    echo "<p>Registro foi incluído.</p>";
+    echo "<a href='consulta.php'>Consultar dados</a><br>";
+    echo "<a href='editar.php'>Editar dados</a><br>";
+    echo "<a href='excluir.php'>Excluir dados</a><br>";
+    echo "<a href='cantina.html'>Voltar ao Início</a>";
+} else {
+    echo "Erro ao incluir o registro: " . $conn->errorInfo()[2];
+}
 
-echo "<p> $result registro foi incluído</p>"; 
-
-echo "<a href='consulta.php'>Consultar dados <br></a>"; 
-
-echo "<a href='editar.php'>Editar dados</a>"; 
-
-echo "<br><a href='excluir.php'>Excluir dados</a>"; 
-
-echo "<br><a href='cantina.html'>Voltar ao Inicio</a>";
-
+$conn = null;
 ?>
